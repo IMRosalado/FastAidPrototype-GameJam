b915482e6ef9a7ba6175e80b8ac9fb3c70699831
Isabela Rosalado <isabelarosaladolp@gmail.com>
2016-08-05 13:14:10 +0800

    + test UI scene + UI prefab

---
+++ Prototype/Assets/Scripts/UI/ResumeButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResumeButton: MonoBehaviour {
+
+	public GameObject pauseMenu;
+
+	public void OnClick(){
+		Time.timeScale = 1;
+		pauseMenu.SetActive (false);
+
+	}
+}
