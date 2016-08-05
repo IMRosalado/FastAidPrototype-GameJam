7d0fc3445264887a22d74751e7059045328b1741
Alvin Carpio <alvincarpio004@gmail.com>
2016-08-05 15:41:41 +0800

    +spawn CS +patient CS +bed CS *bed Prefab 	+added a script

---
+++ Prototype/Assets/Scripts/Bed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bed : MonoBehaviour {
+	public bool mayTao=false;
+
+	void OnTriggerEnter(Collider x){
+		if (x.gameObject.tag=="Patient"){
+			mayTao=true;
+		}
+	}
+	void OnTriggerExit(Collider x){
+		if (x.gameObject.tag=="Patient"){
+			mayTao=false;
+		}
+	}
+
+	void Update(){
+	}
+}
+	
---
+++ Prototype/Assets/Scripts/spawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn : MonoBehaviour {
+
+	public GameObject _spawn;
+	void Start () {
+		StartCoroutine(Spawn(_spawn));
+	}
+
+
+	IEnumerator Spawn(GameObject toSpawn){
+		while(true){
+			GameObject s = (GameObject)Instantiate(toSpawn, new Vector3(-8f, -0.44f, 0), Quaternion.identity);
+			s.SetActive(true);
+			yield return new WaitForSeconds(5f);
+		}
+
+	}
+}
