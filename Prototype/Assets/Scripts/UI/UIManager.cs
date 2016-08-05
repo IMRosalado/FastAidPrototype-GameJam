e62cd4738a78c0ffc487b58cf3e1dc19af0ec63f
Isabela Rosalado <isabelarosaladolp@gmail.com>
2016-08-05 15:24:43 +0800

    + Patient script (decaying life, sprite color depending on injury, speech bubble) + Patient in scene * player script: removed pathfinding (temporary) + lives (lose lives when a patient dies

---
+++ Prototype/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public UISprite[] lives;
+	private int livesLeft = 3;
+	public static UIManager instance;
+
+
+	void Start(){
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+
+	}
+
+	public void loseLife(){
+		lives [3 - livesLeft].spriteName = "grave";
+		livesLeft--;
+
+	}
+}
